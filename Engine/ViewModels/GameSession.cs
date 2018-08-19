6648c889b1b3c912708ba1bcda424b4e1a0b5636
SeanB09 <41342002+SeanB09@users.noreply.github.com>
2018-08-19 03:42:14 +0300

    Created solution, GameScreen, Player Class, GameSession(VeiwModel) Class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        private Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+
+            // Refactored for readability from the tutorial version by removing all
+            // of the CurrentPlayer.Properties
+            CurrentPlayer = new Player
+            {
+                Name = "Scott",
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                Gold = 100000,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+        }
+    }
+}
